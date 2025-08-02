7d21bf939e000576f996a345a7b02df7d57b9f2a
Ritesh Navhal <goku@Riteshs-iMac-Pro.local>
2025-08-02 10:59:14 +0530

    fix: dieat recalls list adn add apis done

--- lib/constant/staticData.dart
+++ lib/constant/staticData.dart
@@ -28,4 +28,24 @@ class StaticData {
     "Running- sprinting": "Running- sprinting",
     "Sailing": "Sailing"
   };
+
+  static const Map<String, String> RECIPES = {
+    "A000002": "Spicy Tomato Rice (Recipe)",
+    "A000004": "Gongura Chicken Curry Recipe - Andhra Style Gongura Chicken",
+    "A000005": "Andhra Style Alam Pachadi Recipe - Adrak Chutney (Recipe)",
+    "A000007": "Udupi Style Ash Gourd Coconut Curry Recipe",
+    "A000013":
+        "Andhra Steel Ingavu Charu Recipe - Asfoitida Flavored Tamarand Rasam",
+    "A000021": "Garlic Amla Chutney Recipe - Lehsun Amla Chutney Recipe",
+    "A000022":
+        "Maharashtrian Kakdi Thalipeeth Recipe - Spiced Cucumber Flat Bread",
+    "A000029": "Mini Dal Mature Chat Recipe - That Perfect Party Snack",
+    "A000045": "Makki and Methi Roti Recipe",
+    "A000046":
+        "Cabbage and Carrot Sambharo Recipe - Gujarati Vegetable Stir Fry",
+    "A000048": "Andhra Steel Tomato Knock Recipe (Tomato Chutney)",
+    "A000058": "Chanar Dal Recipe - Bengali Steel Paneer & Potato Gravy",
+    "A000063": "Iranian Baida Curry Recipe",
+    "A000070": "Dubuk Vade Recipe - Khandeshi Gram Flour Dumpling Curry"
+  };
 }
